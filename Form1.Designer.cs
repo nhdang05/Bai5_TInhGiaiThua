9b14fad79e5bb556734c7da0fbddd4f0b9617614
Administrator <Administrator@LAB1-MAY25>
2025-02-17 11:24:46 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Bai5_TInhGiaiThua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSo = new System.Windows.Forms.Button();
+            this.lblKQ = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "chương trình tính giai thừa";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "nhập số";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "kết quả";
+            // 
+            // txtSo
+            // 
+            this.txtSo.Location = new System.Drawing.Point(118, 82);
+            this.txtSo.Name = "txtSo";
+            this.txtSo.Size = new System.Drawing.Size(137, 21);
+            this.txtSo.TabIndex = 4;
+            this.txtSo.UseVisualStyleBackColor = true;
+            this.txtSo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblKQ
+            // 
+            this.lblKQ.Location = new System.Drawing.Point(118, 155);
+            this.lblKQ.Name = "lblKQ";
+            this.lblKQ.Size = new System.Drawing.Size(137, 21);
+            this.lblKQ.TabIndex = 5;
+            this.lblKQ.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(207, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 31);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "tính giai thừa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(387, 82);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(159, 45);
+            this.trackBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 368);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.lblKQ);
+            this.Controls.Add(this.txtSo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button txtSo;
+        private System.Windows.Forms.Button lblKQ;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai5_TInhGiaiThua
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            int so = Convert.ToInt32(txtso.Text);
+            int gt = 1;
+            for (int i = 1; i <= so; i++)
+            {
+                gt *= i;
+            }
+            lblKQ.Text = "Kết quả:" + gt.ToString();
+        }
+        private void txtso_Click(object sender, EventArgs e)
+        {
+            int so = Convert.ToInt32(lblso.Text);
+            int gt = 1;
+            for (int i = 1; i <= so; i++)
+            {
+                gt *= i;
+            }
+            lblKQ.Text = "kết quả:" + gt.ToString();
+        }
+        private void trbTinhTB_Scroll(object sender, EventArgs e)
+        {
+            txtso.Text = trbTinhGT.Value.ToString();
+            int so = Convert.ToInt32(txtso.Text);
+            int gt = 1;
+            for (int i = 1; i <= so; i++)
+            {
+                gt *= i;
+            }
+            lblKQ.Text = "kết quả:" + gt.ToString();
+        }
+        private void txtso_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void lblKQ_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
